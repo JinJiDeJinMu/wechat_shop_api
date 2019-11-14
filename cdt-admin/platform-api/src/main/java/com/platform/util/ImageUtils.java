2ace99b01b7fc3ca9584e16f206588f84b69811b
RoyalYuan <303687320@qq.com>
2019-11-14 16:47:34 +0800

    修复jdk类包 冲突

--- cdt-admin/platform-api/src/main/java/com/platform/util/ImageUtils.java
+++ cdt-admin/platform-api/src/main/java/com/platform/util/ImageUtils.java
@@ -1,12 +1,15 @@
 package com.platform.util;
 
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGImageEncoder;
+//import com.sun.image.codec.jpeg.JPEGCodec;
+//import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.AttributedString;
@@ -185,13 +188,20 @@ public class ImageUtils {
             //绘制图像  getScaledInstance表示创建此图像的缩放版本，返回一个新的缩放版本Image,按指定的width,height呈现图像
             //Image.SCALE_SMOOTH,选择图像平滑度比缩放速度具有更高优先级的图像缩放算法。
             tag.getGraphics().drawImage(src.getScaledInstance(widthdist, heightdist, Image.SCALE_SMOOTH), 0, 0, null);
+
             //创建文件输出流
-            FileOutputStream out = new FileOutputStream(imgdist);
-            //将图片按JPEG压缩，保存到out中
-            JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
-            encoder.encode(tag);
-            //关闭文件输出流
-            out.close();
+//            FileOutputStream out = new FileOutputStream(imgdist);
+//            //将图片按JPEG压缩，保存到out中
+//            JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+//            encoder.encode(tag);
+//            //关闭文件输出流
+//            out.close();
+
+            //FileOutputStream out = new FileOutputStream(dstName);
+            //JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+            //encoder.encode(dstImage);
+            String formatName = imgdist.substring(imgdist.lastIndexOf(".") + 1);
+            ImageIO.write(tag, /*"GIF"*/ formatName /* format desired */, new File(imgdist) /* target */);
         } catch (Exception ef) {
             ef.printStackTrace();
         }
