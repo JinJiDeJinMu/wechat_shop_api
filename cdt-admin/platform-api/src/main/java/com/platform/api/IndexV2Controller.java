183aeb31bdadfea306a52bf71446753532f417af
chensu <chensu_98@163.com>
2019-11-15 15:18:30 +0800

    新首页

--- cdt-admin/platform-api/src/main/java/com/platform/api/IndexV2Controller.java
+++ cdt-admin/platform-api/src/main/java/com/platform/api/IndexV2Controller.java
@@ -1,9 +1,24 @@
 package com.platform.api;
 
+import com.chundengtai.base.result.Result;
+import com.github.pagehelper.PageHelper;
+import com.platform.annotation.IgnoreAuth;
+import com.platform.entity.*;
+import com.platform.service.*;
+import com.platform.util.ApiBaseAction;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 窝边生活<br>
@@ -12,7 +27,113 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v2/index")
 @Slf4j
-public class IndexV2Controller {
+public class IndexV2Controller extends ApiBaseAction {
+    @Autowired
+    private ApiAdService adService;
+    @Autowired
+    private ApiChannelService channelService;
+    @Autowired
+    private ApiGoodsService goodsService;
+    @Autowired
+    private ApiBrandService brandService;
+    @Autowired
+    private ApiTopicService topicService;
+    @Autowired
+    private ApiCategoryService categoryService;
+    @Autowired
+    private ApiCartService cartService;
+    @Autowired
+    private LifeServiceSer lifeServiceSer;
 
+    /**
+     * app首页
+     */
+    @ApiOperation(value = "首页")
+    @IgnoreAuth
+    @GetMapping(value = "index")
+    public Object index() {
+        Map<String, Object> resultObj = new HashMap<String, Object>();
+        Map<String, Object> param = new HashMap<String, Object>();
+        param.put("ad_position_id", 1);
+        List<AdVo> banner = adService.queryList(param);
+        List<AdVo> hotProduct = getCollectByHotType(banner);
+        List<AdVo> activity = getCollectByActiveType(banner);
 
+        resultObj.put("hotProduct", hotProduct);
+        resultObj.put("activity", activity);
+        //
+        param = new HashMap<String, Object>();
+        param.put("is_new", 1);
+        param.put("is_delete", 0);
+        param.put("is_on_sale", 1);
+        param.put("fields", "id, name, list_pic_url, retail_price");
+        PageHelper.startPage(0, 4, false);
+        List<GoodsVo> newGoods = goodsService.queryList(param);
+        resultObj.put("newGoodsList", newGoods);
+        //
+        param = new HashMap<String, Object>();
+        param.put("is_hot", "1");
+        param.put("is_on_sale", 1);
+        param.put("is_delete", 0);
+        PageHelper.startPage(0, 3, false);
+        List<GoodsVo> hotGoods = goodsService.queryHotGoodsList(param);
+        resultObj.put("hotGoodsList", hotGoods);
+        // 当前购物车中
+        List<CartVo> cartList = new ArrayList<CartVo>();
+        if (null != getUserId()) {
+            //查询列表数据
+            Map<String, Object> cartParam = new HashMap<String, Object>();
+            cartParam.put("user_id", getUserId());
+            cartList = cartService.queryList(cartParam);
+        }
+        if (null != cartList && cartList.size() > 0 && null != hotGoods && hotGoods.size() > 0) {
+            for (GoodsVo goodsVo : hotGoods) {
+                for (CartVo cartVo : cartList) {
+                    if (goodsVo.getId().equals(cartVo.getGoods_id())) {
+                        goodsVo.setCart_num(cartVo.getNumber());
+                    }
+                }
+            }
+        }
+
+        param = new HashMap<String, Object>();
+        param.put("parent_id", 0);
+        param.put("notName", "推荐");//<>
+        List<CategoryVo> categoryList = categoryService.queryList(param);
+        List<Map<String, Object>> newCategoryList = new ArrayList<>();
+
+        for (CategoryVo categoryItem : categoryList) {
+            param.remove("fields");
+            param.put("parent_id", categoryItem.getId());
+            List<CategoryVo> categoryEntityList = categoryService.queryList(param);
+            List<Integer> childCategoryIds = new ArrayList<>();
+            for (CategoryVo categoryEntity : categoryEntityList) {
+                childCategoryIds.add(categoryEntity.getId());
+            }
+
+            param = new HashMap<String, Object>();
+            param.put("categoryIds", childCategoryIds);
+            param.put("fields", "id as id, name as name, list_pic_url as list_pic_url, retail_price as retail_price");
+            PageHelper.startPage(0, 7, false);
+            List<GoodsVo> categoryGoods = goodsService.queryList(param);
+            Map<String, Object> newCategory = new HashMap<String, Object>();
+            newCategory.put("id", categoryItem.getId());
+            newCategory.put("name", categoryItem.getName());
+            newCategory.put("goodsList", categoryGoods);
+            newCategoryList.add(newCategory);
+        }
+        resultObj.put("categoryList", newCategoryList);
+        return Result.success(resultObj);
+    }
+
+    private List<AdVo> getCollectByHotType(List<AdVo> banner) {
+        return banner.stream().filter(b->
+            b.getType()=="爆品展示"
+        ).collect(Collectors.toList());
+    }
+    private List<AdVo> getCollectByActiveType(List<AdVo> banner) {
+        return banner.stream().filter(b->
+                b.getType()=="活动展示"
+        ).collect(Collectors.toList());
+    }
 }
