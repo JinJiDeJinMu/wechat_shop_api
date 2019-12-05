163ee0d6807476e86ae3649ebcdd0b7daf2d00ff
RoyalYuan <303687320@qq.com>
2019-12-05 16:41:44 +0800

    订单支付订单支付6

--- cdt-admin/platform-api/src/main/java/com/platform/api/ApiPayController.java
+++ cdt-admin/platform-api/src/main/java/com/platform/api/ApiPayController.java
@@ -521,7 +521,7 @@ public class ApiPayController extends ApiBaseAction {
 
     private void orderStatusLogic(String out_trade_no) {
         OrderVo orderItem = orderService.queryOrderNo(out_trade_no);
-
+        if (orderItem == null) orderItem = orderService.queryOrderNo(out_trade_no);
         log.info("微信回调设置=====out_trade_no>:" + out_trade_no);
         log.info("微信回调Json=====>:" + JSON.toJSONString(orderItem));
         orderItem.setOrder_sn(out_trade_no);
