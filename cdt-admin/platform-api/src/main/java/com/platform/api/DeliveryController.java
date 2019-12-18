a7efe423ab06454af8be641a2ef21ed0c44dd700
RoyalYuan <303687320@qq.com>
2019-12-18 17:19:32 +0800

    增加物流属性配置2

--- cdt-admin/platform-api/src/main/java/com/platform/api/DeliveryController.java
+++ cdt-admin/platform-api/src/main/java/com/platform/api/DeliveryController.java
@@ -1,7 +1,12 @@
 package com.platform.api;
 
 import cn.hutool.http.HttpUtil;
+import com.chundengtai.base.bean.Delivery;
 import com.chundengtai.base.common.Json;
+import com.chundengtai.base.service.DeliveryService;
+import com.platform.entity.OrderVo;
+import com.platform.service.ApiOrderService;
+import dto.DeliveryDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -18,9 +23,9 @@ public class DeliveryController {
 
     @Autowired
     private DeliveryService deliveryService;
-    @Autowired
-    private OrderService orderService;
 
+    @Autowired
+    private ApiOrderService orderService;
     /**
      * 查看物流接口
      */
@@ -29,12 +34,14 @@ public class DeliveryController {
     @ApiImplicitParam(name = "orderNumber", value = "订单号", required = true, dataType = "String")
     public ResponseEntity<DeliveryDto> checkDelivery(String orderNumber) {
 
-        Delivery delivery = deliveryService.getById(order.getDvyId());
-        String url = delivery.getQueryUrl().replace("{dvyFlowId}", order.getDvyFlowId());
+        OrderVo order = orderService.queryOrderNo(orderNumber);
+        Delivery delivery = deliveryService.getById(orderNumber);
+        //String url = delivery.getQueryUrl().replace("{dvyFlowId}", order.getDvyFlowId());
+        String url = delivery.getQueryUrl().replace("{dvyFlowId}", order.getShipping_id().toString());
         String deliveryJson = HttpUtil.get(url);
 
         DeliveryDto deliveryDto = Json.parseObject(deliveryJson, DeliveryDto.class);
-        deliveryDto.setDvyFlowId(order.getDvyFlowId());
+        //deliveryDto.setDvyFlowId(order.getDvyFlowId());
         deliveryDto.setCompanyHomeUrl(delivery.getCompanyHomeUrl());
         deliveryDto.setCompanyName(delivery.getDvyName());
         return ResponseEntity.ok(deliveryDto);
