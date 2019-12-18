12b91477f3b9075f6c9c7513eb277370d4a46501
RoyalYuan <303687320@qq.com>
2019-12-18 17:19:57 +0800

    增加物流属性配置3

--- cdt-admin/platform-api/src/main/java/com/platform/api/DeliveryController.java
+++ cdt-admin/platform-api/src/main/java/com/platform/api/DeliveryController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/delivery")
+@RequestMapping("/api/delivery")
 @Api(tags = "查看物流接口")
 public class DeliveryController {
 
