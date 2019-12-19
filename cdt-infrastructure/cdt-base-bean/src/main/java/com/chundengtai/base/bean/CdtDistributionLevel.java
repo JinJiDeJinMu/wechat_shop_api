8985314aee02c4e9ee8b8d8b1eb45623428e103a
Administrator <chensu_98@163.com>
2019-12-19 10:10:19 +0800

    字段修改

--- cdt-infrastructure/cdt-base-bean/src/main/java/com/chundengtai/base/bean/CdtDistributionLevel.java
+++ cdt-infrastructure/cdt-base-bean/src/main/java/com/chundengtai/base/bean/CdtDistributionLevel.java
@@ -2,12 +2,13 @@ package com.chundengtai.base.bean;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * <p>
@@ -20,6 +21,7 @@ import java.time.LocalDateTime;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
+@TableName(value = "cdt_distribution_level")
 public class CdtDistributionLevel implements Serializable {
 
     private static final long serialVersionUID=1L;
@@ -53,7 +55,7 @@ public class CdtDistributionLevel implements Serializable {
     /**
      * 创建时间
      */
-    private LocalDateTime createdTime;
+    private Date createdTime;
 
     /**
      * 校验token
