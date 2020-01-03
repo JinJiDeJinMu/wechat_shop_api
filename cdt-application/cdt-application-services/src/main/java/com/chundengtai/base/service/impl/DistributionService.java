0fc1bc6b6b380f29cd649ed18f653553eebcb474
Administrator <chensu_98@163.com>
2020-01-03 11:33:26 +0800

    分销bug测试修改9

--- cdt-application/cdt-application-services/src/main/java/com/chundengtai/base/service/impl/DistributionService.java
+++ cdt-application/cdt-application-services/src/main/java/com/chundengtai/base/service/impl/DistributionService.java
@@ -409,7 +409,7 @@ public class DistributionService implements IdistributionService {
 
         BiConsumer<CdtDistridetail, Order> userSumeryOp = (CdtDistridetail detailModel, Order orderModel) -> {
             CdtDistributionLevel item = distributionLevelService.getOne(new QueryWrapper<CdtDistributionLevel>().lambda()
-                    .eq(CdtDistributionLevel::getUserId, detailModel.getGoldUserId()));
+                    .eq(CdtDistributionLevel::getUserId, detailModel.getUserId()));
 
             log.info("============BiConsumer====================>进入表达式逻辑");
             if (item == null) {
