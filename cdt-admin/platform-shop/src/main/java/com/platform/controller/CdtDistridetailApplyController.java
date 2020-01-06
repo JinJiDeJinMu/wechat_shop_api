923e141ccc36135eb74a53f93edb6aa2fd383ecb
Administrator <chensu_98@163.com>
2020-01-06 14:50:04 +0800

    分销提现修改7

--- cdt-admin/platform-shop/src/main/java/com/platform/controller/CdtDistridetailApplyController.java
+++ cdt-admin/platform-shop/src/main/java/com/platform/controller/CdtDistridetailApplyController.java
@@ -87,36 +87,33 @@ public class CdtDistridetailApplyController {
     //@RequiresPermissions("cdtdistridetailapply:deleteModel")
     public R review(@RequestBody Integer[] ids) {
         List<CdtDistridetailApply> list = cdtDistridetailApplyService.listByIds(Arrays.asList(ids));
-        list = list.stream().filter(e -> e.getStatus() == 405).collect(Collectors.toList());
+        list = list.stream().filter(e -> e.getStatus() == 403).collect(Collectors.toList());
         log.info("======" + list);
-        list.forEach(e -> {
-                    String weixinOpenid = e.getWeixinOpenid();
-                    String realName = e.getRealName();
-                    double money = e.getMoney().doubleValue();
-            log.info("===开始审核===" + e);
-                    if (applyCashService.wechatMoneyToUser(weixinOpenid, realName, money)) {
-                        try {
-                            //更新分销审核表类型
-                            e.setStatus(DistributionStatus.COMPLETED_GETGOLD.getCode());
-                            e.setUpdateTime(new Date());
-                            e.setOperator(ShiroUtils.getUserEntity().getUsername());
-                            cdtDistridetailApplyService.updateById(e);
-                            //更新分销订单类型
-                            CdtDistridetail cdtDistridetail = cdtDistridetailService.getById(e.getId());
-                            cdtDistridetail.setUpdateTime(new Date());
-                            cdtDistridetail.setStatus(DistributionStatus.COMPLETED_GETGOLD.getCode());
-                            /*cdtDistridetail.setId(null);
-                            cdtDistridetail.setToken(null);
-                            String token = JavaWebToken.createJavaWebToken(BeanJwtUtil.javabean2map(cdtDistridetail));
-                            cdtDistridetail.setToken(token);
-                            cdtDistridetail.setId(e.getId());*/
-                            cdtDistridetailService.updateById(cdtDistridetail);
-                        } catch (Exception e1) {
-                            e1.printStackTrace();
-                        }
+        if (list != null) {
+            list.forEach(e -> {
+                String weixinOpenid = e.getWeixinOpenid();
+                String realName = e.getRealName();
+                double money = e.getMoney().doubleValue();
+                log.info("===开始审核===" + e);
+                if (applyCashService.wechatMoneyToUser(weixinOpenid, realName, money)) {
+                    try {
+                        //更新分销审核表类型
+                        e.setStatus(DistributionStatus.COMPLETED_GETGOLD.getCode());
+                        e.setUpdateTime(new Date());
+                        e.setOperator(ShiroUtils.getUserEntity().getUsername());
+                        cdtDistridetailApplyService.updateById(e);
+                        //更新分销订单类型
+                        CdtDistridetail cdtDistridetail = cdtDistridetailService.getById(e.getId());
+                        cdtDistridetail.setUpdateTime(new Date());
+                        cdtDistridetail.setStatus(DistributionStatus.COMPLETED_GETGOLD.getCode());
+                        cdtDistridetailService.updateById(cdtDistridetail);
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
                     }
+                }
 
-                });
+            });
+        }
         return R.ok();
     }
 }
