bf9522f92ab8c514260910d309e6588109c32445
RoyalYuan <303687320@qq.com>
2020-01-09 20:55:54 +0800

    修复参数修改错误2

--- cdt-wx-server/src/main/java/com/platform/util/ApiUserUtils.java
+++ cdt-wx-server/src/main/java/com/platform/util/ApiUserUtils.java
@@ -3,9 +3,7 @@ package com.platform.util;
 import com.platform.utils.ResourceUtil;
 
 /**
- * 作者: @author Harmon <br>
- * 时间: 2017-08-11 08:58<br>
- * 描述: ApiUserUtils <br>
+ * 描述: ApiUserUtils
  */
 public class ApiUserUtils {
 
