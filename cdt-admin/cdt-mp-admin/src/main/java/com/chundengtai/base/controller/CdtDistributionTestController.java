4af965cf118907cfb10f0cb9ee9655efef769772
Administrator <chensu_98@163.com>
2020-01-13 14:55:02 +0800

    测试接口

---
+++ cdt-admin/cdt-mp-admin/src/main/java/com/chundengtai/base/controller/CdtDistributionTestController.java
@@ -0,0 +1,129 @@
+package com.chundengtai.base.controller;
+
+import com.chundengtai.base.bean.Order;
+import com.chundengtai.base.facade.IdistributionFacade;
+import com.chundengtai.base.service.OrderService;
+import com.chundengtai.base.util.CommonUtil;
+import com.chundengtai.base.weixinapi.GoodsTypeEnum;
+import com.chundengtai.base.weixinapi.OrderStatusEnum;
+import com.chundengtai.base.weixinapi.PayTypeEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * @Description TODO
+ * @Author hujinbiao
+ * @Date 2020年1月13日 0013 上午 09:55:15
+ * @Version 1.0
+ **/
+@RestController
+@RequestMapping("/test")
+public class CdtDistributionTestController {
+
+    @Autowired
+    private IdistributionFacade idistributionFacade;
+
+    @Autowired
+    private OrderService orderService;
+
+
+    @GetMapping("/referreRelation")
+    public void referreRelation(long userId, String referrerEncpyt) {
+
+        idistributionFacade.referreRelation(userId, referrerEncpyt);
+    }
+
+    @GetMapping("/recordDistributeLog")
+    public void recordDistributeLog(Integer userId, Order order) {
+
+        idistributionFacade.recordDistributeLog(userId, order);
+    }
+
+    @GetMapping("/notifyOrderStatus")
+    public void notifyOrderStatus(Integer userId, Order order, GoodsTypeEnum goodsTypeEnum) {
+
+        idistributionFacade.notifyOrderStatus(userId, order, goodsTypeEnum);
+    }
+
+    @GetMapping("/order")
+    public Order order(Integer userId, Integer goodType) {
+
+        Order order = getOrder(userId, goodType);
+        orderService.save(order);
+        return order;
+    }
+
+    @GetMapping("/pay")
+    public void payOrder(Integer orderId) {
+
+        Order order = orderService.getById(orderId);
+        order.setOrderStatus(OrderStatusEnum.PAYED_ORDER.getCode());
+        order.setPayStatus(PayTypeEnum.PAYED.getCode());
+        boolean result = orderService.updateById(order);
+        if (result) {
+            idistributionFacade.recordDistributeLog(order.getUserId(), order);
+        }
+
+    }
+
+    @GetMapping("/changeorder")
+    public void changeOrderstatus(Integer orderId) {
+
+        Order order = orderService.getById(orderId);
+        order.setOrderStatus(OrderStatusEnum.COMPLETED_ORDER.getCode());
+        order.setConfirmTime(new Date());
+        Boolean result = orderService.updateById(order);
+        if (result) {
+            idistributionFacade.notifyOrderStatus(order.getUserId(), order, GoodsTypeEnum.getEnumByKey(order.getGoodsType()));
+        }
+
+    }
+
+    public Order getOrder(Integer userId, Integer goodType) {
+        Order order = new Order();
+        BigDecimal orderTotalPrice = new BigDecimal(5);
+        // 总订单编号
+        order.setAllOrderId(UUID.randomUUID().toString().replaceAll("-", ""));
+        order.setOrderSn(CommonUtil.generateOrderNumber());
+        order.setUserId(userId);
+        order.setOrderType("2");
+        // 收货地址和运费
+        order.setConsignee(null);
+        order.setMobile(null);
+        order.setCountry(null);
+        order.setProvince(null);
+        order.setCity(null);
+        order.setDistrict(null);
+        order.setAddress(null);
+        // 留言
+        order.setPostscript(null);
+        // 使用的优惠券
+        order.setCouponId(1);
+        order.setCouponPrice(null);
+        // 订单金额
+        order.setAddTime(new Date());
+        order.setGoodsPrice(orderTotalPrice);
+        order.setOrderPrice(orderTotalPrice);
+        order.setActualPrice(orderTotalPrice);
+        order.setAllPrice(orderTotalPrice);
+        order.setFreightPrice(orderTotalPrice);
+        order.setGoodsType(goodType);
+        // 设置为待付款状态
+        order.setOrderStatus(OrderStatusEnum.WAIT_PAY.getCode());
+        order.setShippingStatus(0);
+        order.setPayStatus(0);
+        order.setShippingId(0);
+        order.setShippingFee(orderTotalPrice);
+        order.setIntegral(0);
+        order.setIntegralMoney(new BigDecimal(0));
+        order.setMerchantId(16L);
+        return order;
+    }
+
+}
