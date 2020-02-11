01f09bece8bd764541d3c98b922668bed86fca0f
jinmu <hmlnzn@163.com>
2020-02-11 20:34:20 +0800

    修改测试4

--- cdt-infrastructure/cdt-base-bean/src/main/java/com/chundengtai/base/bean/CdtUserScore.java
+++ cdt-infrastructure/cdt-base-bean/src/main/java/com/chundengtai/base/bean/CdtUserScore.java
@@ -1,5 +1,7 @@
 package com.chundengtai.base.bean;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -26,6 +28,7 @@ public class CdtUserScore implements Serializable {
     /**
      * 用户id
      */
+    @TableId(value = "id",type = IdType.INPUT)
     private Long id;
 
     /**
